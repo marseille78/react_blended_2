c679bfce65f3176a41ac9a843b84d61ac4aade0a
RuslanBoiko <boyko.ruslan78@gmail.com>
2023-05-14 11:12:08 +0300

    Add query

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,43 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.value.trim()) {
+      alert('Введіть щось будь ласка :)');
+      return;
+    }
+
+    this.props.onSubmit(this.state.value);
+    this.setState({ value:  '' });
+  }
+
   render() {
-    return <h2>SearchForm</h2>;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -2,11 +2,35 @@ import { Component } from 'react';
 
 import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
+import { getImages } from 'service/image-service';
 
 export class Gallery extends Component {
+
+  state = {
+    query: '',
+    page: 1,
+    list: [],
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    const { query, page } = this.state;
+
+    if (prevState.query !== query || prevState.page !== page) {
+      getImages(query, page)
+        .then(data => console.log(data));
+    }
+  }
+
+  handleSubmit = (query) => {
+    this.setState({
+      query,
+    });
+  }
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={ this.handleSubmit } />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
