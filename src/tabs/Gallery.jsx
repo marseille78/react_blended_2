2d0b0df8ed0d8e2992985e901702ecda47b9a465
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-05-14 10:46:27 +0200

    added task2

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -5,33 +5,52 @@ import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 import { getImages } from 'service/image-service';
 
 export class Gallery extends Component {
-
   state = {
     query: '',
     page: 1,
     list: [],
-  };
+    showLoadMore: false,
+    isEmpti: false,
+   };
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     const { query, page } = this.state;
 
     if (prevState.query !== query || prevState.page !== page) {
-      getImages(query, page)
-        .then(data => console.log(data));
+      getImages(query, page).then(({ photos, total_results }) => {
+        if(!photos.length) {
+          this.setState({isEmpti: true})
+          return
+        }
+        this.setState(({ list }) => ({ list: [...list, ...photos],
+        showLoadMore: page < Math.ceil(total_results/15)
+        }));
+      });
     }
   }
 
-  handleSubmit = (query) => {
+  handleSubmit = query => {
     this.setState({
       query,
     });
-  }
+  };
 
   render() {
+    const { list, showLoadMore, isEmpti } = this.state;
     return (
       <>
-        <SearchForm onSubmit={ this.handleSubmit } />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <SearchForm onSubmit={this.handleSubmit} />
+        <Grid>
+          {list.map(({ id, alt, src, avg_color }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {showLoadMore && <Button>Load more</Button>}
+        {isEmpti && <Text textAlign="center">Sorry. There are no images ... 😭</Text>}
       </>
     );
   }
